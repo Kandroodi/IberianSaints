acadf3a30753f154116832169ff8a5700686a940
Kandroodi <m.rostamikandroodi@let.ru.nl>
2021-12-17 13:31:53 +0100

    media folder setting, issue #23, #24

--- iberianSaints/urls.py
+++ iberianSaints/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from django.conf import settings
+
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url('', include(('saints.urls', 'saints'), namespace="saints")),
     path("select2/", include("django_select2.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
